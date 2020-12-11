13b5ff1df0903178bed7af429d871882b10574b3
maparrar <maparrar@gmail.com>
2020-12-11 08:22:49 -0500

    Must work :)

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/skillsviewerTT/i);
+  const linkElement = screen.getByText(/skillsviewer/i);
   expect(linkElement).toBeInTheDocument();
 });
