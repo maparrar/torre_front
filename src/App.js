4e932d8dcb474187ba55bf1377cea674cd4f8536
maparrar <maparrar@gmail.com>
2020-12-11 09:03:58 -0500

    Initial scaffolding

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
+import Header from "./components/Header/Header";
+import Body from "./components/Body/Body";
+import Viewer from "./containers/Viewer/Viewer";
+
 function App() {
   return (
-    <div className="App">
-      <h1>skillsviewer</h1>
-    </div>
+    <>
+      <Header />
+      <Body>
+        <Viewer />
+      </Body>
+    </>
   );
 }
 
---
+++ src/components/Body/Body.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Body = props => (
+  <>{props.children}</>
+);
+
+
+
+export default Body;
